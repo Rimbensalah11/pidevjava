360891f3519fd6848ff911e36815988e5d1d2c7b
melekmsakni <ouesletyyosra@gmail.com>
2023-02-15 22:04:11 +0100

    yosra's task

---
+++ src/projetJava/Projetjava.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projetJava;
+
+import edu.pijava.utils.MyConnection;
+import edu.pijava.services.ReservationCrud;
+import edu.pijava.model.Reservation;
+import edu.pijava.model.Facture;
+import edu.pijava.services.FactureCrud;
+
+/**
+ *
+ * @author ouesl
+ */
+public class Projetjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //MyConnection conn = new MyConnection.getInstance();
+
+        Reservation r1 = new Reservation(8, 2, 5, 3);
+        Facture f1 = new Facture(7, 8, 80.5f, "yosra");
+
+        ReservationCrud res = new ReservationCrud();
+
+        FactureCrud ress = new FactureCrud();
+
+        res.ajouterReservation(r1);
+        ress.ajouterFacture(f1);
+        //ress.supprimerFacture(1);
+        res.annulerReservation(8);
+        System.out.println(res.afficherReservation());
+        System.out.println(ress.afficherFacture());
+
+    }
+
+}
